fb02fbaec40155069be47ce5fd6378769c67a310
Niu Liu <liuniu@tsinghua.org.cn>
2025-05-13 09:56:44 +1200

    remove testCancelJob.sh

--- src/test/scripts/testCancelJob.sh
+++
@@ -1,9 +0,0 @@
-curl -sS -X POST "http://172.17.0.1:18080/api/aigamefactory/canceljob" \
-     -H "Content-Type: application/json; charset=utf-8" \
-     -d '{"job_id":"sZSCxHpiEY"}' \
-     -o body.json \
-     -D headers.txt \
-     -w '%{http_code}\n' \
-     > status.txt \
-     2> curl.err
-
